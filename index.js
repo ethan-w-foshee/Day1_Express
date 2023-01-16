39e786164b7ddd2a277d442a7af492f3b5551c26
ethan.w.foshee <skyhailer2@gmail.com>
2023-01-16 15:09:30 -0600

    Get, Post, Put, and Delete all function

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 
 const express = require('express')
+const bPars = require('body-parser')
 const app = express()
 const port = process.env.PORT || 4000
 
 const { users } = require('./state')
-
+app.use(express.json())
 /* BEGIN - create routes here */
+app.get('/users', (req, res) => {
+  res.json(users)
+})
 
+app.get(`/users/:userId`, (req, res) => {
+  res.json(users[req.params.userId - 1])
+})
+
+app.post('/users', (req, res) => {
+  req.body._id = users.length + 1
+  users.push(req.body)
+  res.send(users)
+})
+
+app.put(`/users/:userId`, (req, res) => {
+    req.body._id = users[req.params.userId - 1]._id
+    users[req.params.userId - 1] = req.body
+    res.send(users)
+})
+
+app.delete(`/users/:userId`, (req, res) => {
+  users[req.params.userId].isActive = false
+  res.send("Deleted")
+})
 
 /* END - create routes here */
 
